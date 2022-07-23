fad4f899d651c69a8b58e8bfd766487d90e72791
Nami Arj <namiarjmandi@gmail.com>
2022-07-23 07:41:08 +0000

    Add files via upload

--- src/threadpool.c
+++ src/threadpool.c
@@ -1,35 +1,37 @@
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "threadpool.h"
 
-struct task_data {
-	void (*run_tasks)(tpool_t, void *);
-	void *arg;
+#define DEFAULT_QUEUE_SIZE 64
+#define TASK_S_SIZE sizeof(struct task_s)
+
+struct task_s {
+    void (*run_tasks)(tpool_t, void *);
+    void *arg;
 };
 
 struct tpool {
-	struct task_data*	task_queue;
-	int			queue_head, queue_tail;
-	int			num_threads;
-	int			active_threads;
-	int			scheduled;
-	int			keepalive;
-	pthread_mutex_t		mutex;
-	pthread_t		*threads;
-	pthread_cond_t 		notify;
-	pthread_cond_t 		done;
+	struct task_s	*task_queue;
+	unsigned int	queue_size, queue_head, queue_tail;
+	unsigned int	num_threads, active_threads, pending;
+	bool		keepalive;
+	pthread_mutex_t	mutex;
+	pthread_t	*threads;
+	pthread_cond_t 	notify, done;
 };
 
 void
 *run_tasks(void *arg)
 {
 	tpool_t pool = arg;
-	struct task_data picked_task;
+	struct task_s task;
 loop:
 	pthread_mutex_lock(&pool->mutex);
-	while (!pool->scheduled && pool->keepalive) {
+	while (!pool->pending && pool->keepalive) {
 		pool->active_threads--;
 		if (!pool->active_threads)
 			pthread_cond_signal(&pool->done);
@@ -38,10 +40,10 @@ loop:
 	}
 	if (!pool->keepalive)
 		goto shutdown;
-	picked_task = pool->task_queue[pool->queue_head++];
-	pool->scheduled--;
+	task = pool->task_queue[pool->queue_head++];
+	pool->pending--;
 	pthread_mutex_unlock(&pool->mutex);
-	picked_task.run_tasks(pool,(void *)picked_task.arg);
+	task.run_tasks(pool,(void *)task.arg);
 	goto loop;
 shutdown:
 	pthread_mutex_unlock(&pool->mutex);
@@ -54,11 +56,12 @@ tpool_t
 tpool_create(unsigned int num_threads)
 {
 	tpool_t pool = malloc(sizeof(struct tpool));
-	pool->task_queue = malloc(sizeof(struct task_data));
-	pool->queue_head = pool->queue_tail = pool->scheduled = 0;
+	pool->queue_size = DEFAULT_QUEUE_SIZE;
+	pool->task_queue = malloc(TASK_S_SIZE * pool->queue_size);
+	pool->queue_head = pool->queue_tail = pool->pending = 0;
 	pool->active_threads = pool->num_threads = num_threads;
 	pool->threads = malloc(sizeof(pthread_t) * num_threads);
-	pool->keepalive = 1;
+	pool->keepalive = true;
 	pthread_mutex_init(&pool->mutex, NULL);
 	for (int i = 0; i < num_threads; i++)
 		pthread_create(&pool->threads[i], NULL, &run_tasks, pool);
@@ -66,15 +69,31 @@ tpool_create(unsigned int num_threads)
 }
 
 void
+resize_queue(tpool_t pool)
+{
+	struct task_s *resized_queue = malloc(TASK_S_SIZE * pool->queue_size);
+	pool->queue_tail -= pool->queue_head; 
+	memcpy(resized_queue, &pool->task_queue[pool->queue_head], TASK_S_SIZE * pool->queue_tail);
+	pool->queue_head = 0;
+	free(pool->task_queue);
+	pool->task_queue = resized_queue;
+}
+
+void
 tpool_schedule_task(tpool_t pool, void (*fun)(tpool_t, void*), void *arg)
 {
-	struct task_data task;
+	struct task_s task;
+	struct task_s *resized_queue;
 	task.run_tasks = fun;
 	task.arg = (void *)arg;
 	pthread_mutex_lock(&pool->mutex);
 	pool->task_queue[pool->queue_tail++] = task;
-	pool->scheduled++;
-	pool->task_queue = realloc(pool->task_queue, sizeof(struct task_data) * (pool->queue_tail + 1));
+	pool->pending++;
+	if (pool->queue_tail == pool->queue_size) {
+		pool->queue_size *= 2;
+		resize_queue(pool);
+	} else if (pool->queue_head > pool->queue_size / 2)
+		resize_queue(pool);
 	pthread_mutex_unlock(&pool->mutex);
 	pthread_cond_signal(&pool->notify);
 }
@@ -83,12 +102,12 @@ void
 tpool_join(tpool_t pool)
 {
 	pthread_mutex_lock(&pool->mutex);
-	while (pool->active_threads || pool->scheduled)
+	while (pool->active_threads || pool->pending)
 		pthread_cond_wait(&pool->done, &pool->mutex);
-	pool->keepalive = 0;
+	pool->keepalive = false;
 	pthread_cond_broadcast(&pool->notify);
 	pthread_mutex_unlock(&pool->mutex);
-	for (int i = 0; i < pool->num_threads; i++)
+	for (unsigned int i = 0; i < pool->num_threads; i++)
 		pthread_join(pool->threads[i], NULL);
 	free(pool->threads); 
 	free(pool->task_queue); 
--- src/threadpool.h
+++ src/threadpool.h
@@ -2,5 +2,6 @@
 
 typedef struct tpool *tpool_t;
 tpool_t tpool_create(unsigned int num_threads);
+void resize_queue(tpool_t pool);
 void tpool_schedule_task(tpool_t pool, void (*fun)(tpool_t, void *), void *arg);
 void tpool_join(tpool_t pool);
