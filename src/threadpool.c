99d550018397eac2f7494806aebdf04cd08a61e8
Nami Arj <namiarjmandi@gmail.com>
2022-07-31 21:07:20 +0000

    Add files via upload

--- src/threadpool.c
+++ src/threadpool.c
@@ -1,3 +1,4 @@
+#include <err.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -16,29 +17,27 @@ struct task_s {
 
 struct tpool {
 	struct task_s	*queue;
-	unsigned int	active, total, head, tail, pending, size;
-	bool		keepalive;
-	pthread_t	*threads;
-	pthread_mutex_t	mutex;
+	unsigned int	size, active, total, head, tail, pending;
+	bool		shutdown;
+	pthread_t	*thread;
+	pthread_mutex_t	lock;
 	pthread_cond_t 	notify, done;
 };
 
 static void
-*guard(char *fun, void *ptr)
+*guard(void *ptr)
 {
-	if (ptr == NULL) {
-		printf("%s returned null", fun);
-		exit(1);
-	}
-	return ptr;
+	if (ptr == NULL)
+		err(1, "returned null");
+	return (ptr);
 }
 
 static void
 resize(tpool_t pool)
 {
-	struct task_s *resized = guard("malloc", malloc(TASK_S_SIZE * pool->size));
+	struct task_s *resized = guard(malloc(TASK_S_SIZE * pool->size));
 	pool->tail -= pool->head; 
-	guard("memcpy", memcpy(resized, &pool->queue[pool->head], TASK_S_SIZE * pool->tail));
+	guard(memcpy(resized, &pool->queue[pool->head], TASK_S_SIZE * pool->tail));
 	pool->head = 0;
 	free(pool->queue);
 	pool->queue = resized;
@@ -50,39 +49,44 @@ static void
 	tpool_t pool = arg;
 	struct task_s task;
 loop:
-	pthread_mutex_lock(&pool->mutex);
-	while (!pool->pending && pool->keepalive) {
+	pthread_mutex_lock(&pool->lock);
+
+	while (!pool->pending && !pool->shutdown) {
 		if (!--pool->active)
 			pthread_cond_signal(&pool->done);
-		pthread_cond_wait(&pool->notify, &pool->mutex);
+		pthread_cond_wait(&pool->notify, &pool->lock);
 		pool->active++;
 	}
-	if (!pool->keepalive)
+
+	if (pool->shutdown)
 		goto shutdown;
+
 	task = pool->queue[pool->head++];
 	pool->pending--;
-	pthread_mutex_unlock(&pool->mutex);
+	pthread_mutex_unlock(&pool->lock);
 	task.run_tasks(pool,(void *)task.arg);
 	goto loop;
 shutdown:
-	pthread_mutex_unlock(&pool->mutex);
+	pthread_mutex_unlock(&pool->lock);
 	pthread_exit(NULL);
 	return (NULL);
 }
 
 tpool_t
-tpool_create(unsigned int num_threads)
+tpool_create(unsigned int num)
 {
-	tpool_t pool = guard("malloc", malloc(sizeof(struct tpool)));
-	pool->active = pool->total = num_threads;
-	pool->head = pool->tail = pool->pending = 0;
+	tpool_t pool = guard(malloc(sizeof(struct tpool)));
+	pool->queue = guard(malloc(TASK_S_SIZE * QUEUE_SIZE));
+	pool->thread = guard(malloc(sizeof(pthread_t) * num));
 	pool->size = QUEUE_SIZE;
-	pool->queue = guard("malloc", malloc(TASK_S_SIZE * pool->size));
-	pool->threads = guard("malloc", malloc(sizeof(pthread_t) * num_threads));
-	pool->keepalive = true;
-	pthread_mutex_init(&pool->mutex, NULL);
-	for (int i = 0; i < num_threads; i++)
-		pthread_create(&pool->threads[i], NULL, &run_tasks, pool);
+	pool->active = pool->total = num;
+	pool->head = pool->tail = pool->pending = 0;
+	pool->shutdown = false;
+	pthread_mutex_init(&pool->lock, NULL);
+
+	for (; num > 0; num--)
+		pthread_create(&pool->thread[num], NULL, &run_tasks, pool);
+
 	return (pool);
 }
 
@@ -92,8 +96,9 @@ tpool_schedule_task(tpool_t pool, void (*fun)(tpool_t, void*), void *arg)
 	struct task_s task;
 	task.run_tasks = fun;
 	task.arg = (void *)arg;
-	pthread_mutex_lock(&pool->mutex);
+	pthread_mutex_lock(&pool->lock);
 	pool->queue[pool->tail++] = task;
+
 	if (++pool->pending < pool->size / 4) {
 		if (pool->size > QUEUE_SIZE)
 			pool->size /= 2;
@@ -102,25 +107,30 @@ tpool_schedule_task(tpool_t pool, void (*fun)(tpool_t, void*), void *arg)
 		pool->size *= 2;
 		resize(pool);
 	}
-	pthread_mutex_unlock(&pool->mutex);
+
+	pthread_mutex_unlock(&pool->lock);
 	pthread_cond_signal(&pool->notify);
 }
 
 void
 tpool_join(tpool_t pool)
 {
-	pthread_mutex_lock(&pool->mutex);
+	pthread_mutex_lock(&pool->lock);
+
 	while (pool->active || pool->pending)
-		pthread_cond_wait(&pool->done, &pool->mutex);
-	pool->keepalive = false;
+		pthread_cond_wait(&pool->done, &pool->lock);
+
+	pool->shutdown = true;
 	pthread_cond_broadcast(&pool->notify);
-	pthread_mutex_unlock(&pool->mutex);
-	for (unsigned int i = 0; i < pool->total; i++)
-		pthread_join(pool->threads[i], NULL);
-	pthread_mutex_destroy(&pool->mutex);
+	pthread_mutex_unlock(&pool->lock);
+
+	for (; pool->total > 0; pool->total--)
+		pthread_join(pool->thread[pool->total], NULL);
+
+	pthread_mutex_destroy(&pool->lock);
 	pthread_cond_destroy(&pool->notify);
 	pthread_cond_destroy(&pool->done);
-	free(pool->threads); 
+	free(pool->thread); 
 	free(pool->queue); 
 	free(pool);
 }
